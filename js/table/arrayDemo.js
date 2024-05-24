d1cabfd43a9475c4eb99eea86d6f2789058c5de3
Radar3759 <chainmailgirl@gmail.com>
2024-05-24 13:25:15 -0700

    -add JS print table files

---
+++ js/table/arrayDemo.js
@@ -0,0 +1,6 @@
+let strings = ["violin", "viola", "bass"];
+let brass = ["trumpet", "tuba", "trombone"];
+let percussion = ["piano", "timpani", "cymbal"];
+
+alert("testing!");
+console.log("testing");
